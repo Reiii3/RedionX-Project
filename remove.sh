2d5f974cf30f62b12e70171d171bc058e6b08a8d
Reiii3 <mohtoyib79@gmail.com>
2024-12-01 08:49:57 +0700

    Maintenance succesfuly

--- remove.sh
+++ remove.sh
@@ -1,1 +1,38 @@
 
+if [ $AXERON = false ]; then
+	echo "Only Support in Laxeron"
+fi
+
+$AXFUN
+import axeron.prop
+echo "─────────────────────────────"
+echo "Removing ${name} (${version})"
+echo "─────────────────────────────"
+echo
+sleep 1
+echo "───────────────────────────────────────────────────────"""
+#// Unistaler By Reii
+    setprop debug.sf.hw 0
+		setprop debug.egl.hw 0
+		setprop debug.egl.sync 1
+		setprop debug.gpuprio 5
+    setprop debug.cpuprio 5
+    setprop debug.ioprio 6
+    setprop debug.performance.tuning 0
+    setprop debug.performance.profile 0
+    setprop debug.performance.force false
+    setprop debug.performance_schema 0
+    setprop debug.power.profile balance
+    setprop debug.multicore.processing 0
+		performance=false
+		setprop debug.composition.type cpu
+    setprop debug.hwui.renderer opengl
+    am force-stop "$runPackage"
+    cmd power set-fixed-performance-mode-enabled false
+    cmd power set-adaptive-power-saver-enabled true
+    cmd power set-mode 1
+    settings delete global updatable_driver_production_opt_in
+  echo "[${runPackage}] Removing Succesfuly"
+  echo "Next Remove Redionpl If installed"
+echo "──────────────────────────────────────────────────────"""
+echo "              Thank you for using my module"
