eee82e582a1f327a7eda3cee90c030ce8bd8537e
Reiii3 <mohtoyib79@gmail.com>
2024-12-03 17:00:55 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -172,7 +172,7 @@ esac
  cmd thermalservice override-status 0
  cmd power set-fixed-performance-mode-enabled true
  cmd power set-adaptive-power-saver-enabled false
- cmd package compile -m speed --secondary-dex -f ${runPackage}
+ cmd package compile -m speed --secondary-dex -f ${runPackage} > /dev/null 2>&1
  cmd power set-mode 0
 echo "├[${runPackage}] Actived Renderer📲"
 sleep 0.5
