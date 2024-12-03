86ded4c364ce2ce836512f902de835e7e6f3a9b5
Reiii3 <mohtoyib79@gmail.com>
2024-12-03 13:39:25 +0700

    update instal.sh

--- instal.sh
+++ instal.sh
@@ -149,6 +149,7 @@ case $1 in
 		setprop debug.cpuprio 6
     setprop debug.gpuprio 7
     setprop debug.ioprio 7
+    setprop debug.rs.default-CPU-driver 1
     setprop debug.performance.tuning 1
     setprop debug.performance.profile 1
     setprop debug.performance.force true
